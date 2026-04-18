b5195c1f627f32e84420899c3d7b3d880af3b708
AsavriKaur <asavri04@gmail.com>
2026-04-18 18:40:44 +1000

    Fixed lint issues in AddImageModal.tsx

--- frontend/src/components/common/slides/AddImageModal.tsx
+++ frontend/src/components/common/slides/AddImageModal.tsx
@@ -27,85 +27,85 @@ interface AddImageModalProps {
 }
 
 const AddImageModal=({onClose,onSubmit,initialData}:AddImageModalProps)=>{
-    const [x, setX] = useState(initialData?.x ?? 0);
-    const [y, setY] = useState(initialData?.y ?? 0);
-    const [width,setWidth]=useState(initialData?.width ??30);
-    const [height,setHeight]=useState(initialData?.height ??30);
-    const [src,setSrc]=useState(initialData?.src ??'');
-    const [alt,setAlt]=useState(initialData?.alt ?? '');
-    const [inputStyle, setInputStyle] = useState(() => {
-        if (!initialData?.src) return 'file';
-        return initialData.src.startsWith('data:') ? 'file' : 'src';
-        });
+  const [x, setX] = useState(initialData?.x ?? 0);
+  const [y, setY] = useState(initialData?.y ?? 0);
+  const [width,setWidth]=useState(initialData?.width ??30);
+  const [height,setHeight]=useState(initialData?.height ??30);
+  const [src,setSrc]=useState(initialData?.src ??'');
+  const [alt,setAlt]=useState(initialData?.alt ?? '');
+  const [inputStyle, setInputStyle] = useState(() => {
+    if (!initialData?.src) return 'file';
+    return initialData.src.startsWith('data:') ? 'file' : 'src';
+  });
 
-    const handleFileStyle=()=>{
-        if (inputStyle=='file'){
-            setInputStyle('src'); 
-        }else{
-            setInputStyle('file'); 
-        }
+  const handleFileStyle=()=>{
+    if (inputStyle=='file'){
+      setInputStyle('src'); 
+    }else{
+      setInputStyle('file'); 
     }
-    return (
-        <>
-            <Dialog open={true} onOpenChange={onClose}>
-            <DialogContent>
-                <DialogHeader>
-                <DialogTitle>{initialData ? 'Edit Image Element' : 'New Image Element'}</DialogTitle>
-                <DialogDescription></DialogDescription>
-                </DialogHeader>
-            <FieldGroup>
-                <Field>
-                    <Label htmlFor="src">src</Label>
-                    {inputStyle === 'file' && (
-                    <>
-                        {src && <img src={src} alt="preview" className="max-h-20 mb-2 w-full h-full object-contain" />}
-                        <Input type="file" accept="image/*" onChange={(e) => {
-                            const file = e.target.files?.[0];
-                            if (!file) return;
-                            const reader = new FileReader();
-                            reader.onload = () => setSrc(reader.result as string);
-                            reader.readAsDataURL(file);
-                        }} />
-                    </>
-                    )}
-                    {inputStyle=='src' &&
+  }
+  return (
+    <>
+      <Dialog open={true} onOpenChange={onClose}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{initialData ? 'Edit Image Element' : 'New Image Element'}</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="src">src</Label>
+              {inputStyle === 'file' && (
+                <>
+                  {src && <img src={src} alt="preview" className="max-h-20 mb-2 w-full h-full object-contain" />}
+                  <Input type="file" accept="image/*" onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    if (!file) return;
+                    const reader = new FileReader();
+                    reader.onload = () => setSrc(reader.result as string);
+                    reader.readAsDataURL(file);
+                  }} />
+                </>
+              )}
+              {inputStyle=='src' &&
                     <Input  type="src" placeholder="Enter url of Image" value={src} onChange={(e) => setSrc((e.target.value))} />
-                    }
-                    <Button onClick={handleFileStyle} variant="outline">
-                        {inputStyle === 'file' ? 'Switch to URL' : 'Switch to File Upload'}
-                    </Button>
-                </Field>
-                <Field>
-                    <Label htmlFor="alt">alt</Label>
-                    <Input  type="text" placeholder="Describe the image" value={alt} onChange={(e) => setAlt((e.target.value))} />
-                </Field>
-                <Field>
-                    <Label htmlFor="width">width</Label>
-                    <Input  type="number" placeholder="Enter your width(0-100)" value={width} onChange={(e) => setWidth(Number(e.target.value))} />
-                </Field>
-                <Field>
-                    <Label htmlFor="height">height</Label>
-                    <Input  type="number" placeholder="Enter your height(0-100)" value={height} onChange={(e) => setHeight(Number(e.target.value))} />
-                </Field>
-                {initialData && <Field>
-                    <Label htmlFor="x-y">position</Label>
-                    <Input  type="number" placeholder="x" value={x} onChange={(e) => setX(Number(e.target.value))} />
-                    <Input  type="number" placeholder="y" value={y} onChange={(e) => setY(Number(e.target.value))} />
-                </Field>
-                }
-            </FieldGroup>
-            <DialogFooter>
-                <DialogClose asChild>
-                    <Button variant="outline" onClick={onClose}>Cancel</Button>
-                </DialogClose>
-                <Button onClick={() => onSubmit( width, height, src, alt,x,y)}>
-                    {initialData ? 'Save' : 'Create'}
-                </Button>
+              }
+              <Button onClick={handleFileStyle} variant="outline">
+                {inputStyle === 'file' ? 'Switch to URL' : 'Switch to File Upload'}
+              </Button>
+            </Field>
+            <Field>
+              <Label htmlFor="alt">alt</Label>
+              <Input  type="text" placeholder="Describe the image" value={alt} onChange={(e) => setAlt((e.target.value))} />
+            </Field>
+            <Field>
+              <Label htmlFor="width">width</Label>
+              <Input  type="number" placeholder="Enter your width(0-100)" value={width} onChange={(e) => setWidth(Number(e.target.value))} />
+            </Field>
+            <Field>
+              <Label htmlFor="height">height</Label>
+              <Input  type="number" placeholder="Enter your height(0-100)" value={height} onChange={(e) => setHeight(Number(e.target.value))} />
+            </Field>
+            {initialData && <Field>
+              <Label htmlFor="x-y">position</Label>
+              <Input  type="number" placeholder="x" value={x} onChange={(e) => setX(Number(e.target.value))} />
+              <Input  type="number" placeholder="y" value={y} onChange={(e) => setY(Number(e.target.value))} />
+            </Field>
+            }
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline" onClick={onClose}>Cancel</Button>
+            </DialogClose>
+            <Button onClick={() => onSubmit( width, height, src, alt,x,y)}>
+              {initialData ? 'Save' : 'Create'}
+            </Button>
           </DialogFooter>
-            </DialogContent>
-        </Dialog>
-        </>
-    )
+        </DialogContent>
+      </Dialog>
+    </>
+  )
 }
 
 export default AddImageModal;
